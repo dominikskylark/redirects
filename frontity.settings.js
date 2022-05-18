fe440bf4057ea492f1e55e3a4ee66ac662f23314
Dominik <dominik@skylarkcreative.co.uk>
2022-05-18 12:28:44 +0100

    Vercel

--- frontity.settings.js
+++ frontity.settings.js
@@ -45,7 +45,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org"
+          "url": "https://relaxed-joliot.77-68-115-165.plesk.page"
         }
       }
     },
